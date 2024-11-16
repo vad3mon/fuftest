58f76593bce2fdbccb2b45035e6703c443cc5861
vad3mon <gg-gg13@bk.ru>
2024-11-16 03:15:12 +0300

    git init

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\API\RegisterController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+//    Route::resource('products', ProductController::class);
+    Route::post('test', [RegisterController::class, 'test'])->name('test');
+});
